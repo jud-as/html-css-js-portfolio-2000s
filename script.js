622528574ebdd2149eee854f19618db3042995f9
jud_as <guilhermeferreira082@gmail.com>
2023-09-04 16:33:03 -0300

    title.txt animation window w/ javascript.

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
-document.getElementById("popup-minimize-button").addEventListener("close-popup", function () {
-    const minimizeButton = document.getElementById("popup-minimize-button");
 
-    minimizeButton.classList.add("button");
+const minimizeButton = document.getElementById('popup-minimize-button');
+const pathButton = document.getElementById('popup-path-button');
+const closeButton = document.getElementById('popup-close-button');
+const logoWindow = document.getElementById('logo-popup');
 
-    setTimeout(function () {
-        minimizeButton.classList.remove("button");
-    }, 1000);
+let cooldown = false;
+
+minimizeButton.addEventListener('click', ()=> {
+
+    if (!cooldown) {
+        cooldown = true;
+    
+        logoWindow.classList.add('transition-height')
+        logoWindow.classList.add('closed');
+        logoWindow.classList.remove('opened');
+        logoWindow.style.height = '0';
+        setTimeout(() => {
+            logoWindow.classList.remove('transition-height');
+            cooldown = false;
+        }, 800);
+    }
+}); 
+
+pathButton.addEventListener('click', ()=> {
+
+    if (!cooldown) {
+        cooldown = true;
+
+        logoWindow.classList.add('transition-height');
+        logoWindow.classList.add('opened');
+        logoWindow.classList.remove('closed');
+        logoWindow.style.height = '6em';
+        setTimeout(() => {
+            logoWindow.classList.remove('transition-height');
+            cooldown = false;
+        }, 800);
+    }
+});
+
+
+closeButton.addEventListener('click', ()=> {
+    
 });
